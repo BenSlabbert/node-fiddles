73f965ca9fc0627f67205c39a1462357763ec562
Ben Slabbert <ben@hex.co.za>
2017-12-10 17:38:07 +0200

    reading file syn and async

--- files-and-fs/app.js
+++ files-and-fs/app.js
@@ -1,4 +1,16 @@
-let fs = require('fs');
+let fs = require( 'fs' );
 
-let greet  = fs.readFileSync(__dirname + '/greet.txt', 'utf8');
-console.log(greet);
+// returns buffer
+let readSync = fs.readFileSync(__dirname + '/greet.txt');
+console.log(readSync);
+
+// returns string
+readSync = fs.readFileSync(__dirname + '/greet.txt', 'utf8');
+console.log(readSync);
+
+// async call to read file with a callback
+let greetAsync = fs.readFile( __dirname + '/greet.txt', 'utf8', ( err, data ) => {
+    console.log( data );
+} );
+
+console.log( 'ola' );
