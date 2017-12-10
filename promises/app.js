5fa0f6d677986040b5d382470fffbfd5cbffdf42
Ben Slabbert <ben@hex.co.za>
2017-12-10 17:19:31 +0200

    formatting

--- promises/app.js
+++ promises/app.js
@@ -1,21 +1,21 @@
 let promises = [];
 
-promises.push(new Promise((resolve, reject) => {
-    resolve('hi1');
-}));
+promises.push( new Promise( ( resolve, reject ) => {
+    resolve( 'hi1' );
+} ) );
 
-promises.push(new Promise((resolve, reject) => {
-    resolve('hi2');
-}));
+promises.push( new Promise( ( resolve, reject ) => {
+    resolve( 'hi2' );
+} ) );
 
-promises.push(new Promise((resolve, reject) => {
-    reject('hi3');
-}));
+promises.push( new Promise( ( resolve, reject ) => {
+    reject( 'hi3' );
+} ) );
 
-promises.push(new Promise((resolve, reject) => {
-    reject('hi4');
-}));
+promises.push( new Promise( ( resolve, reject ) => {
+    reject( 'hi4' );
+} ) );
 
-Promise.all(promises)
-    .catch((error) => { console.log(error) })
-    .then((result) => console.log(result));
+Promise.all( promises ).catch( ( error ) => {
+    console.log( error )
+} ).then( ( result ) => console.log( result ) );
