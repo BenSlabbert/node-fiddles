8df40ae32921d4e5814e6fe220e1ececd9feceab
Ben Slabbert <ben@hex.co.za>
2017-12-10 18:28:08 +0200

    reading and writing with streams

---
+++ streams/app.js
@@ -0,0 +1,14 @@
+let fs = require( 'fs' );
+
+let readable = fs.createReadStream( __dirname + '/data.txt', {
+    encoding: 'utf8',
+    highWaterMark: 16 * 1024
+} );
+
+let writeable = fs.createWriteStream(__dirname + '/copy.txt');
+
+
+readable.on( 'data', function ( chunk ) {
+    console.log( chunk );
+    writeable.write(chunk);
+} );
