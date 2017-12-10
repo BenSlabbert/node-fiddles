1aac2b5bb6633b6391306b0d805845d9c24e8958
Ben Slabbert <ben@hex.co.za>
2017-12-10 20:59:07 +0200

    first http server

---
+++ http-parser/app.js
@@ -0,0 +1,11 @@
+let http = require( 'http' );
+
+http.createServer( function ( req, res ) {
+
+    res.writeHead( 200, {
+        'Content-Type': 'text/plain'
+    } );
+
+    res.end( 'Hello World!\n' );
+
+} ).listen( 9000, '127.0.0.1' );
