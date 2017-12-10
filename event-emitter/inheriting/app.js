601e85057060d7c43c7dad50c313f95b116cb620
Ben Slabbert <ben@hex.co.za>
2017-12-10 13:51:24 +0200

    adding another test method inheriter

--- event-emitter/inheriting/app.js
+++ event-emitter/inheriting/app.js
@@ -5,14 +5,32 @@ function Greeter() {
     this.greeting = 'Hello world!';
 }
 
+function Custom() {
+    this.data = "done";
+}
+
+util.inherits(Custom, EventEmitter);
 util.inherits(Greeter, EventEmitter);
 
-Greeter.prototype.greet = function(data) {
+Custom.prototype.method = function (data) {
+    console.log('hello');
+    this.emit('method', data);
+};
+
+Greeter.prototype.greet = function (data) {
     console.log(this.greeting + ': ' + data);
     this.emit('greet', data);
 };
 
 var greeter1 = new Greeter();
+var cust = new Custom();
+
+cust.on('method', function (data) {
+    console.log('called method method...' + data);
+});
+
+
+cust.method('ola');
 
 greeter1.on('greet', function (data) {
     console.log('A greeting: ' + data);
