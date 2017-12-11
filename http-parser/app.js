e6c646a30d116bbd943c97eb3f9d7913e82e7ece
Ben Slabbert <ben@hex.co.za>
2017-12-11 10:18:47 +0200

    formatting

--- http-parser/app.js
+++ http-parser/app.js
@@ -3,10 +3,7 @@ let fs = require( 'fs' );
 
 http.createServer( function ( req, res ) {
 
-    res.writeHead( 200, {
-        'Content-Type': 'text/html'
-    } );
-
+    res.writeHead( 200, { 'Content-Type': 'text/html' } );
     fs.createReadStream( __dirname + '/index.htm' ).pipe( res );
 
 } ).listen( 9000, '127.0.0.1' );
