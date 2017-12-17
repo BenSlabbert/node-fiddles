0211b9da612f3d0526e2c62fd4218e3ecbf35468
Ben Slabbert <ben@hex.co.za>
2017-12-17 13:37:59 +0200

    simple adding of users with node + mongoose to mongo docker

--- mongodb/app.js
+++ mongodb/app.js
@@ -1,5 +1,37 @@
-let express = require('express');
+let express = require( 'express' );
 let app = express();
-let mongoose = require('mongoose');
+let mongoose = require( 'mongoose' );
 
-mongoose.connect('mongodb://localhost:27017/addresses');
+mongoose.connect( 'mongodb://localhost:27017/addressbook' );
+
+let Schema = mongoose.Schema;
+
+let personSchema = new Schema( {
+    firstname: String,
+    lastname: String,
+    address: String
+} );
+
+let Person = mongoose.model( 'Person', personSchema );
+
+let john = Person( {
+    firstname: "John",
+    lastname: 'Doe',
+    address: ' 1st Street'
+} );
+
+john.save( function ( err ) {
+    if ( err ) {
+        console.log( err );
+    } else {
+        console.log( 'it worked!' );
+    }
+} );
+
+Person.find( {}, function ( err, users ) {
+    if ( err ) {
+        console.log( err );
+    } else {
+        console.log( users );
+    }
+} );
